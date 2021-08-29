b86d1f3504e20c34275c5e501f20cf6df193194d
leehm00 <leehm0073@icloud.com>
2021-08-29 10:47:33 +0800

    对象和类学习

---
+++ javatest/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {//依次为访问修饰符,关键字,返回类型,方法名,类名,参数
+        System.out.println("Hello World");
+    }
+}
---
+++ javatest/Puppy.java
@@ -0,0 +1,33 @@
+public class Puppy {
+    int puppyAge;
+   public Puppy(String name){
+      // 这个构造器仅有一个参数：name
+      System.out.println("小狗的名字是 : " + name ); 
+   }
+ 
+   public void setAge( int age ){
+       puppyAge = age;
+   }
+ 
+   public int getAge( ){
+       System.out.println("小狗的年龄为 : " + puppyAge ); 
+       return puppyAge;
+   }
+ 
+   public static void main(String[] args){
+      /* 创建对象 */
+      Puppy myPuppy = new Puppy( "tommy" );
+      /* 通过方法来设定age */
+      myPuppy.setAge( 2 );
+      /* 调用另一个方法获取age */
+      myPuppy.getAge( );
+      /*你也可以像下面这样访问成员变量 */
+      System.out.println("直接访问变量值 : " + myPuppy.puppyAge ); 
+   }
+}
+/*一个源文件中只能有一个 public 类
+一个源文件可以有多个非 public 类
+源文件的名称应该和 public 类的类名保持一致。例如：源文件中 public 类的类名是 Employee，那么源文件应该命名为Employee.java。
+如果一个类定义在某个包中，那么 package 语句应该在源文件的首行。
+如果源文件包含 import 语句，那么应该放在 package 语句和类定义之间。如果没有 package 语句，那么 import 语句应该在源文件中最前面。
+import 语句和 package 语句对源文件中定义的所有类都有效。在同一源文件中，不能给不同的类不同的包声明。*/
